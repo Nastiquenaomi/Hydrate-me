530136c28858089a7567383be64d72b15d5b33ce
Nastiquenaomi <naominastic030@gmail.com>
2024-11-11 15:27:40 +0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,14 +1,18 @@
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
   e.preventDefault();
-  const data = {
-    username: document.getElementById('username').value,
-    password: document.getElementById('password').value,
-  };
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
 
   const response = await fetch('/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data),
+    body: JSON.stringify({ username, password }),
   });
 
-  const result = await response
+  const result = await response.json();
+  if (result.message === 'Login successful') {
+    window.location.href = 'location.html';
+  } else {
+    document.getElementById('message').innerText = result.message;
+  }
+});
