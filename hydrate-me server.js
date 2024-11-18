60613b9e16191672c2664891de3b794df104b423
tmjoris <tangajoris@gmail.com>
2024-11-18 15:57:13 +0300

    sign up logic

--- hydrate-me server.js
+++ hydrate-me server.js
@@ -38,6 +38,7 @@ app.get('/login', (req, res) => {
   res.render('login');
 });
 
+
 app.get('/register', (req, res) => {
   res.render('register');
 });
@@ -49,6 +50,33 @@ app.get('/result', (req, res) => {
 app.get('/signup', (req, res) => {
   res.render('signup');
 });
+
+app.post('/signup', async (req, res) => {
+    
+  const data = {
+      name: req.body.name,
+      password: req.body.password
+  }
+
+  const checking = await LogInCollection.findOne({ name: req.body.name })
+
+ try{
+  if (checking.name === req.body.name && checking.password===req.body.password) {
+      res.send("user details already exists")
+  }
+  else{
+      await LogInCollection.insertMany([data])
+  }
+ }
+ catch{
+  res.send("wrong inputs")
+ }
+
+  res.status(201).render("home", {
+      naming: req.body.name
+  })
+})
+
 
 
 
